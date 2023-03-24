3cd10a6330b439f919e676699b125474d57d4c49
yqw <347626572@qq.com>
2023-03-24 19:54:57 +0800

    algorithm first commit.

---
+++ src/main/java/com/yqw/algorithm/tree/BinaryTreeDemo.java
@@ -0,0 +1,10 @@
+package com.yqw.algorithm.tree;
+
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+        System.out.println("我的代码");
+        System.out.println("我的代码");
+        System.out.println("我的代码");
+        System.out.println("我的代码");
+    }
+}
